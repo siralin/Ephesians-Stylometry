1fde8f19208929ebc957dd11be2837f8101d9fc6
Elizabeth Kelly <elankel@gmail.com>
2025-09-13 16:36:26 -0400

    keep the better few bigram graphs

--- delta-2-gram-like-beowulf.py
+++ delta-2-gram-like-beowulf.py
@@ -10,8 +10,17 @@ all_text = ""
 for book, book_text in books.items():
   all_text += book_text + " "
 
+# these are best with ward, euclidean
 ngram_size = 2
 num_bigrams_wanted = 10
+linkage_algorithm = 'ward'
+distance_metric = 'euclidean'
+
+# https://aclanthology.org/W15-0709.pdf claims cosine is best (for french, german, english)
+#ngram_size = 2
+#num_bigrams_wanted = 30
+#linkage_algorithm = 'average'
+#distance_metric = 'cosine'
 
 interesting_bigrams = beowulf_utils.find_interesting_bigrams(all_text, num_bigrams_wanted, ngram_size)
 bigram_to_book_to_frequency = beowulf_utils.calculate_ngram_frequencies(interesting_bigrams, books)
@@ -19,7 +28,7 @@ bigram_to_book_to_norm_freq = beowulf_utils.normalize_ngram_frequencies(bigram_t
 
 # TODO not sure if 2d array is oriented correctly here
 new_matrix = [list(row) for row in zip(*bigram_to_book_to_norm_freq)]
-Z = linkage(new_matrix, method='ward',metric='euclidean')
+Z = linkage(new_matrix, method=linkage_algorithm,metric=distance_metric)
 
 plt.figure(figsize=(25, 10))
 plt.title('Hierarchical Clustering Dendrogram (Bigrams)')
@@ -36,7 +45,8 @@ x_labels = ax.get_xmajorticklabels()
 for x in x_labels:
   x.set_color(beowulf_utils.get_label_color(x.get_text()))
 
-plt.savefig('dendrogram_' + str(ngram_size) + 'gram-' + str(num_bigrams_wanted) + '.png', format='png', dpi=100)
+filename = '-'.join(['dendrogram', str(ngram_size) + 'gram', str(num_bigrams_wanted), linkage_algorithm, distance_metric])
+plt.savefig(filename + '.png', format='png', dpi=100)
 plt.show()
 
 # differences from my 2-gram method:
