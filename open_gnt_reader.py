c47ac4d2f7d3c22fdc1168783c5af56de3d000eb
Elizabeth Kelly <elankel@gmail.com>
2025-09-25 16:22:40 -0400

    remove bits of john and mark

--- open_gnt_reader.py
+++ open_gnt_reader.py
@@ -11,17 +11,31 @@ books = [int(val.split('｜')[0][1:]) for val in data['〔Book｜Chapter｜Verse
 # "Greek word of OGNT in unaccented form"
 words = [val.split('｜')[1] for val in data['〔OGNTk｜OGNTu｜OGNTa｜lexeme｜rmac｜sn〕']]
 
+# Find the bits of Mark and John that should be excluded, and exclude them.
 preceding_punctuation = [val.split('｜')[0][1:] for val in data['〔PMpWord｜PMfWord〕']]
 following_punctuation = [val.split('｜')[1][:-1] for val in data['〔PMpWord｜PMfWord〕']]
 
 book_names = ['matthew', 'mark', 'luke', 'john', 'acts', 'romans', '1 corinthians', '2 corinthians', 'galatians', 'ephesians', 'philippians', 'colossians', '1 thessalonians', '2 thessalonians', '1 timothy', '2 timothy', 'titus', 'philemon', 'hebrews', 'james', '1 peter', '2 peter', '1 john', '2 john', '3 john', 'jude', 'revelation']
 
 book_to_text = {}
+skip = False
 for book_index, word, prec_punc, foll_punc in zip(books, words, preceding_punctuation, following_punctuation):
   book = book_names[book_index - 40]
   if book not in book_to_text:
     book_to_text[book] = ''
-  book_to_text[book] += word + ' '
+    if skip:
+      print('error: never ended skip')
+  if '[[' in prec_punc:
+    if skip:
+      print('error: nested [[')
+    skip = True
+  elif not skip:
+    book_to_text[book] += word + ' '
+  elif ']]' in foll_punc:
+    skip = False
+
+if skip:
+  print('error: never ended skip')
 
 for book, text in book_to_text.items():
   with open(os.path.join('opengnt_books', book + ".txt"), 'w') as file:
