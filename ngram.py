a46272e450eaf5eea6d408eca18652d8408c04fb
Elizabeth Kelly <elankel@gmail.com>
2025-09-26 18:57:30 -0400

    pull another method into ngram_utils

--- ngram.py
+++ ngram.py
@@ -1,4 +1,4 @@
-from ngram_utils import ngram_counts_to_normalized_frequencies
+from ngram_utils import calculate_normalized_ngram_frequencies
 from text_normalization_utils import read_normalized_texts
 from ngram_scatter_plot_utils import generate_scatter_plot
 from ngram_dendrogram_utils import generate_dendrogram, check_dendrogram_valid
@@ -15,32 +15,9 @@ book_to_text = read_normalized_texts()
 if MERGE_WORDS:
   for book, text in book_to_text.items():
     book_to_text.update({book: "".join(text.split())})
-print(book_to_text)
 
-# first, calculate frequency of every possible ngram in each book.
-# This is a list of Counters, one for each ngram
-book_to_ngram_counts = [None] * len(book_to_text)
-overall_ngram_counts = Counter()
-
-for index, book in enumerate(book_to_text):
-  text = book_to_text[book]
-  book_to_ngram_counts[index] = Counter()
-  for i in range(0, len(text) - 1):
-    ngram = text[i:i + NGRAM_SIZE]
-
-    # can remove ngrams containing spaces if you want
-    if " " not in ngram:
-      book_to_ngram_counts[index][ngram] += 1
-      overall_ngram_counts[ngram] += 1
-
-# ngrams: a List of the most frequent ngrams
-#
-# book_to_normalized_ngram_frequency: a 2d List[book index][ngram index]
-# where the book index matches the index of the same book in book_to_ngram_counts and book_to_text
-# and the ngram index matches the index of the same ngram in ngrams.
-book_to_normalized_ngram_frequency, ngrams = ngram_counts_to_normalized_frequencies(
-  NUM_NGRAMS_WANTED, book_to_ngram_counts, overall_ngram_counts, NORMALIZATION_METHOD)
-
+book_to_normalized_ngram_frequency, ngrams = calculate_normalized_ngram_frequencies(
+  book_to_text, NUM_NGRAMS_WANTED, NGRAM_SIZE, NORMALIZATION_METHOD)
 
 # In Python 3.7+, dictionary iteration order is always the same,
 # so the keys() call will produce book titles in the right order.
--- ngram_utils.py
+++ ngram_utils.py
@@ -6,10 +6,44 @@ from statistics import stdev, fmean
 # and the book index matches the index of the same book in the given book_to_ngram_counts
 # and the ngram index matches the index of the same ngram in the returned list of ngrams.
 #
-# num_ngrams: int, the number of most common ngrams the zscores should be calculated for
+# book_to_text: Dict of book title to all the text in that book, appropriately normalized.  May or may not contain whitespace.
+# num_ngrams: int, the number of most common ngrams the frequencies should be calculated for
+# normalization_method: whether to normalize frequencies by 'zscore' or 'simple' method
+def calculate_normalized_ngram_frequencies(book_to_text, num_ngrams, ngram_size, normalization_method):
+  # first, calculate frequency of every possible ngram in each book.
+  # This is a list of Counters, one for each ngram
+  book_to_ngram_counts = [None] * len(book_to_text)
+  overall_ngram_counts = Counter()
+
+  for index, book in enumerate(book_to_text):
+    text = book_to_text[book]
+    book_to_ngram_counts[index] = Counter()
+    for i in range(0, len(text) - 1):
+      ngram = text[i:i + ngram_size]
+
+      # can remove ngrams containing spaces if you want
+      if " " not in ngram:
+        book_to_ngram_counts[index][ngram] += 1
+        overall_ngram_counts[ngram] += 1
+
+  # ngrams: a List of the most frequent ngrams
+  #
+  # book_to_normalized_ngram_frequency: a 2d List[book index][ngram index]
+  # where the book index matches the index of the same book in book_to_ngram_counts and book_to_text
+  # and the ngram index matches the index of the same ngram in ngrams.
+  return _ngram_counts_to_normalized_frequencies(
+    num_ngrams, book_to_ngram_counts, overall_ngram_counts, normalization_method)
+
+
+# Returns a tuple of a 2d array and a list of ngrams
+# where array[book index][ngram index] = zscore
+# and the book index matches the index of the same book in the given book_to_ngram_counts
+# and the ngram index matches the index of the same ngram in the returned list of ngrams.
+#
+# num_ngrams: int, the number of most common ngrams the frequencies should be calculated for
 # book_to_ngram_counts: List<Counter>, the number of times every ngram appears in each book
 # overall_ngram_counts: Counter, the number of times every ngram appears in all books
-def ngram_counts_to_normalized_frequencies(num_ngrams, book_to_ngram_counts, overall_ngram_counts, normalization_method):
+def _ngram_counts_to_normalized_frequencies(num_ngrams, book_to_ngram_counts, overall_ngram_counts, normalization_method):
 
   # List of up to num_ngrams ngrams that appear the most frequently overall.
   most_frequent_ngrams = [x[0] for x in overall_ngram_counts.most_common(num_ngrams)]
