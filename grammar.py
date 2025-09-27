a2c9d9f8a60b57d286e9e50f9853a69848f001f5
Elizabeth Kelly <elankel@gmail.com>
2025-09-27 17:12:03 -0400

    script for part-ngrams, and image results

---
+++ grammar.py
@@ -0,0 +1,36 @@
+from grammar_utils import calculate_normalized_part_of_speech_ngram_frequencies
+from read_text_utils import read_parts_of_speech
+from scatter_plot_utils import generate_scatter_plot
+from dendrogram_utils import generate_dendrogram, check_dendrogram_valid
+import matplotlib.pyplot as plt
+
+# In this script, an ngram is a sequence of parts of speech
+# TODO check that when NGRAM_SIZE = 1 this matches part.py
+
+# TODO feel free to change all these parameters
+NGRAM_SIZE = 2
+NUM_NGRAMS_WANTED = 40
+NORMALIZATION_METHOD = 'simple' # 'zscore' or 'simple'
+LINKAGE_ALGORITHM = 'ward'
+DISTANCE_METRIC = 'euclidean'
+
+book_to_text = read_parts_of_speech()
+
+# ngrams: a List of the most frequent ngrams
+#
+# book_to_normalized_ngram_frequency: a 2d List[book index][ngram index]
+# where the book index matches the index of the same book in book_to_ngram_counts and book_to_text
+# and the ngram index matches the index of the same ngram in ngrams.
+book_to_normalized_ngram_frequency, ngrams = calculate_normalized_part_of_speech_ngram_frequencies(
+  book_to_text, NUM_NGRAMS_WANTED, NGRAM_SIZE, NORMALIZATION_METHOD)
+
+# In Python 3.7+, dictionary iteration order is always the same,
+# so the keys() call will produce book titles in the right order.
+generate_scatter_plot(book_to_normalized_ngram_frequency, book_to_text.keys())
+title = str(NUM_NGRAMS_WANTED) + " most frequent " + str(NGRAM_SIZE) + "part-grams (" + NORMALIZATION_METHOD + ")"
+plt.gca().update({"title":title})
+plt.show()
+
+generate_dendrogram(book_to_normalized_ngram_frequency, book_to_text.keys(), LINKAGE_ALGORITHM, DISTANCE_METRIC)
+print(check_dendrogram_valid())
+plt.show()
--- grammar_utils.py
+++ grammar_utils.py
@@ -7,25 +7,23 @@ from unit_frequency_utils import unit_counts_to_normalized_frequencies
 # and the book index matches the index of the same book in the given book_to_ngram_counts
 # and the ngram index matches the index of the same ngram in the returned list of ngrams.
 #
-# book_to_text: Dict of book title to all the text in that book, appropriately normalized.  May or may not contain whitespace.
+# book_to_text: Dict of book title to the parts of speech of all the text in that book.
 # num_ngrams: int, the number of most common ngrams the frequencies should be calculated for
 # normalization_method: whether to normalize frequencies by 'zscore' or 'simple' method
-def calculate_normalized_ngram_frequencies(book_to_text, num_ngrams, ngram_size, normalization_method):
+def calculate_normalized_part_of_speech_ngram_frequencies(book_to_text, num_ngrams, ngram_size, normalization_method):
   # first, calculate frequency of every possible ngram in each book.
   # This is a list of Counters, one for each ngram
   book_to_ngram_counts = [None] * len(book_to_text)
   overall_ngram_counts = Counter()
 
   for index, book in enumerate(book_to_text):
-    text = book_to_text[book]
+    parts = book_to_text[book].split(' ')
     book_to_ngram_counts[index] = Counter()
-    for i in range(0, len(text) - 1):
-      ngram = text[i:i + ngram_size] # TODO WRONG MATH FOR OTHER NGRAM_SIZES
+    for i in range(0, len(parts) - ngram_size + 1):
+      ngram = ' '.join(parts[i:i + ngram_size])
 
-      # can remove ngrams containing spaces if you want
-      if " " not in ngram:
-        book_to_ngram_counts[index][ngram] += 1
-        overall_ngram_counts[ngram] += 1
+      book_to_ngram_counts[index][ngram] += 1
+      overall_ngram_counts[ngram] += 1
 
   return unit_counts_to_normalized_frequencies(
     num_ngrams, book_to_ngram_counts, overall_ngram_counts, normalization_method)
