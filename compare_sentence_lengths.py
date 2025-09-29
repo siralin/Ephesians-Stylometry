a657bd6e7f0b3f89d8fbb068f471e42661329a2a
Elizabeth Kelly <elankel@gmail.com>
2025-09-29 16:07:55 -0400

    compare sentence lengths in line chart

---
+++ compare_sentence_lengths.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from sentence_length_utils import read_sentence_lengths
+from general_utils import BOOK_NAMES, UNCONTESTED_PAUL_BOOKS, CONTESTED_PAUL_BOOKS
+import sys
+import functools as f
+from operator import add
+
+unsorted_book_to_sentence_lengths = read_sentence_lengths()
+book_to_sentence_lengths = dict([(b, unsorted_book_to_sentence_lengths[b]) for b in UNCONTESTED_PAUL_BOOKS])
+book_to_sentence_lengths['all pauline'] = f.reduce(add, book_to_sentence_lengths.values())
+book_to_sentence_lengths['ephesians'] = unsorted_book_to_sentence_lengths['ephesians']
+
+# Find max sentence length.
+# Decide on buckets for sentence lengths (1-5 words, 6-10, etc).
+max_lengths = [max(x) for x in book_to_sentence_lengths.values()]
+max_length = max(max_lengths)
+#if max_length != 166:
+#  print("error: max length is " + str(max_length))
+#  sys.exit()
+
+# Let's go with buckets with a range of 10.
+
+fig = plt.figure()
+ax = fig.add_axes([0.1, 0.1, 0.7, 0.8])
+
+for book, sentence_lengths in book_to_sentence_lengths.items():
+  num_sentences = len(sentence_lengths)
+
+  # desired format: list of number of sentences (divided by total sentences) in each bucket
+  buckets = [0] * 19
+  for sentence_length in sentence_lengths:
+    buckets[(sentence_length - 1) // 10] += 1
+  frequency_buckets = [b / num_sentences for b in buckets]
+
+  linestyle = 'dotted'
+  if book in UNCONTESTED_PAUL_BOOKS:
+    linestyle = 'solid'
+  elif book in CONTESTED_PAUL_BOOKS:
+    linestyle = 'dashed'
+
+  ax.plot(range(1, 20), frequency_buckets, label=book, linestyle=linestyle)
+  ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+plt.show()
--- sentence_length_utils.py
+++ sentence_length_utils.py
@@ -4,6 +4,7 @@ from read_text_utils import read_non_normalized_texts
 def _get_sentence_lengths(text):
   return [sen.count(' ') + 1 for sen in re.split(r'[\.\;]', text)]
 
+# Returns a dict of each book name to a list of the length of each sentence in the book, in order.
 def read_sentence_lengths():
   book_to_text = read_non_normalized_texts()
   return {book: _get_sentence_lengths(text) for book, text in book_to_text.items()}
