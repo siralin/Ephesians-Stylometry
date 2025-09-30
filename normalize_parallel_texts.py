7df8154a00c3c8195427bfe2cfee028bd90a6b8b
Elizabeth Kelly <elankel@gmail.com>
2025-09-29 20:17:51 -0400

    normalize parallel text to capital

--- normalize_parallel_texts.py
+++ normalize_parallel_texts.py
@@ -1,13 +1,13 @@
 import os
 from general_utils import FILE_TYPE_SUFFIX, NORMALIZED_FILE_SUFFIX
 
-def normalize_parallel_texts():
-  for filename in os.listdir('netbible_parallels'):
-    text = ''
-    if NORMALIZED_FILE_SUFFIX in filename:
-      with open(os.path.join(TEXT_DIRECTORY, filename), 'r') as handle:
-        text = handle.read()
+for filename in os.listdir('netbible_parallels'):
+  print(filename)
+  text = ''
+  if NORMALIZED_FILE_SUFFIX in filename:
+    with open(os.path.join('netbible_parallels', filename), 'r') as handle:
+      text = handle.read()
 
     text = text.upper()
-    with open(os.path.join(TEXT_DIRECTORY, filename), 'w') as handle:
-      file.write(text)
+    with open(os.path.join('netbible_parallels', filename), 'w') as handle:
+      handle.write(text)
